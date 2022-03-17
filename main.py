8d31073f3acb00858ccc40ac5352effcd2d91ffb
Omar <omar.moustafa@toolsandapps4us.site>
2022-03-17 21:53:50 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# we will store the numbers here
+num_list = []
+
+# this function will add a number to the list
+def ask_num():
+    # ask for a number
+    num = int(input("Enter numbers: "))
+# run the function for asking numbers
+ask_num()
